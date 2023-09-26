9c67e5d8f1e196510a2d859b86e10c9e016388f5
mashrabburanov <mashrabburanov@gmail.com>
2023-09-26 10:49:58 +0600

    Changed buttons creation
    Buttons now created without createaButton method

--- src/Viewer.java
+++ src/Viewer.java
@@ -74,10 +74,26 @@ class Viewer {
         int row = 0;
         int column = 0;
 
-        Button buttonClearEntry = createButton("\u2190");
-        Button buttonOpeningParentheses = createButton("(");
-        Button buttonClosingParentheses = createButton(")");
-        Button buttonPlus = createButton("+");
+        Button buttonClearEntry = new Button("\u2190");
+        Button buttonOpeningParentheses = new Button("(");
+        Button buttonClosingParentheses = new Button(")");
+        Button buttonPlus = new Button("+");
+
+        buttonClearEntry.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonClearEntry.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonClearEntry.setOnAction(controller);
+
+        buttonOpeningParentheses.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonOpeningParentheses.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonOpeningParentheses.setOnAction(controller);
+        
+        buttonClosingParentheses.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonClosingParentheses.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonClosingParentheses.setOnAction(controller);
+
+        buttonPlus.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonPlus.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonPlus.setOnAction(controller);
 
         grid.add(buttonClearEntry, column++, row);
         grid.add(buttonOpeningParentheses, column++, row);
@@ -87,10 +103,26 @@ class Viewer {
         row = 1;
         column = 0;
 
-        Button buttonDigit7 = createButton("7");
-        Button buttonDigit8 = createButton("8");
-        Button buttonDigit9 = createButton("9");
-        Button buttonMinus = createButton("-");
+        Button buttonDigit7 = new Button("7");
+        Button buttonDigit8 = new Button("8");
+        Button buttonDigit9 = new Button("9");
+        Button buttonMinus = new Button("-");
+
+        buttonDigit7.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonDigit7.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonDigit7.setOnAction(controller);
+
+        buttonDigit8.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonDigit8.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonDigit8.setOnAction(controller);
+
+        buttonDigit9.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonDigit9.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonDigit9.setOnAction(controller);
+
+        buttonMinus.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonMinus.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonMinus.setOnAction(controller);
 
         grid.add(buttonDigit7, column++, row);
         grid.add(buttonDigit8, column++, row);
@@ -100,10 +132,26 @@ class Viewer {
         row = 2;
         column = 0;
 
-        Button buttonDigit4 = createButton("4");
-        Button buttonDigit5 = createButton("5");
-        Button buttonDigit6 = createButton("6");
-        Button buttonStar = createButton("*");
+        Button buttonDigit4 = new Button("4");
+        Button buttonDigit5 = new Button("5");
+        Button buttonDigit6 = new Button("6");
+        Button buttonStar = new Button("*");
+
+        buttonDigit4.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonDigit4.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonDigit4.setOnAction(controller);
+
+        buttonDigit5.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonDigit5.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonDigit5.setOnAction(controller);
+
+        buttonDigit6.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonDigit6.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonDigit6.setOnAction(controller);
+
+        buttonStar.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonStar.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonStar.setOnAction(controller);
 
         grid.add(buttonDigit4, column++, row);
         grid.add(buttonDigit5, column++, row);
@@ -113,10 +161,26 @@ class Viewer {
         row = 3;
         column = 0;
 
-        Button buttonDigit1 = createButton("1");
-        Button buttonDigit2 = createButton("2");
-        Button buttonDigit3 = createButton("3");
-        Button buttonSlash = createButton("/");
+        Button buttonDigit1 = new Button("1");
+        Button buttonDigit2 = new Button("2");
+        Button buttonDigit3 = new Button("3");
+        Button buttonSlash = new Button("/");
+
+        buttonDigit1.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonDigit1.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonDigit1.setOnAction(controller);
+
+        buttonDigit2.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonDigit2.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonDigit2.setOnAction(controller);
+
+        buttonDigit3.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonDigit3.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonDigit3.setOnAction(controller);
+
+        buttonSlash.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonSlash.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonSlash.setOnAction(controller);
 
         grid.add(buttonDigit1, column++, row);
         grid.add(buttonDigit2, column++, row);
@@ -126,9 +190,21 @@ class Viewer {
         row = 4;
         column = 1;
 
-        Button buttonDigit0 = createButton("0");
-        Button buttonDot = createButton(".");
-        Button buttonEqual = createButton("=");
+        Button buttonDigit0 = new Button("0");
+        Button buttonDot = new Button(".");
+        Button buttonEqual = new Button("=");
+
+        buttonDigit0.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonDigit0.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonDigit0.setOnAction(controller);
+
+        buttonDot.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonDot.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonDot.setOnAction(controller);
+
+        buttonEqual.setPrefWidth(BUTTON_DEFAULT_WIDTH);
+        buttonEqual.setPrefHeight(BUTTON_DEFAULT_HEIGHT);
+        buttonEqual.setOnAction(controller);
 
         grid.add(buttonDigit0, column++, row);
         grid.add(buttonDot, column++, row);
@@ -145,21 +221,5 @@ class Viewer {
         grid.setHgap(GRID_GAP);
 
         return grid;
-    }
-
-    private Button createButton(String label) {
-        return createButton(label, BUTTON_DEFAULT_WIDTH, BUTTON_DEFAULT_HEIGHT);
-    }
-
-    private Button createButton(String label,
-                                double preferableWidth,
-                                double preferableHeight) {
-        Button b = new Button(label);
-
-        b.setPrefWidth(preferableWidth);
-        b.setPrefHeight(preferableHeight);
-        b.setOnAction(controller);
-
-        return b;
     }
 }
